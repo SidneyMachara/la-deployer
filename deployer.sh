74538e87b4057fa7edf164a6f04d553dd22ca818
Sidney_machara (Bad_Apple) <sidneymachara@gmail.com>
2019-04-19 13:55:55 +0200

    -

--- deployer.sh
+++ deployer.sh
@@ -53,41 +53,46 @@ echo_white(){
 # sudo apt-get update
 # sudo apt-get -y install php7.2 php7.2-cli php7.2-common php7.2-json php7.2-opcache php7.2-mysql php7.2-mbstring  php7.2-zip php7.2-fpm php7.2-xml composer unzip
 # #sudo apt-get install php7.2 php7.2-cli php7.2-common php7.2-json php7.2-opcache php7.2-mysql php7.2-mbstring php7.2-mcrypt php7.2-zip php7.2-fpm composer unzip
-
-
-
+#
+#
+#
+# echo
+# echo_purple 'copying updated php.ini file (/la-deployer/php.ini /etc/php/7.2/fpm/php.ini) change made = (cgi.fix_pathinfo=0)'
+# sudo rm /etc/php/7.2/fpm/php.ini
+# cp php.ini /etc/php/7.2/fpm/php.ini
+#
+# echo
+# echo_purple 'restart php7.2-fpm'
+# sudo systemctl restart php7.2-fpm
+#
+#
+# #if i can find sudo nano /etc/nginx/sites-available/default -->re install nginx
 echo
-echo_purple 'copying updated php.ini file (/la-deployer/php.ini /etc/php/7.2/fpm/php.ini) change made = (cgi.fix_pathinfo=0)'
-sudo rm /etc/php/7.2/fpm/php.ini
-cp php.ini /etc/php/7.2/fpm/php.ini
-
-echo
-echo_purple 'restart php7.2-fpm'
-sudo systemctl restart php7.2-fpm
-
-exit
-#if i can find sudo nano /etc/nginx/sites-available/default -->re install nginx
-echo
-echo 'copying updated /etc/nginx/sites-available/default'
-cp /la-deployer/default /etc/nginx/sites-available/default
+echo_purple 'copying updated /etc/nginx/sites-available/default'
+sudo rm /etc/nginx/sites-available/default
+cp default /etc/nginx/sites-available/default
 
 echo
 echo 'making sure it is error free'
 sudo nginx -t
 
 
+
 echo
-echo 'now to let it take effect you can restart Nginx'
+echo_purple 'now to let it take effect you can restart Nginx'
 sudo systemctl reload nginx
 
 echo
 echo 'restart nginx'
 sudo service nginx restart
 
+
+
 echo
-echo 'Creating A Folder for Laravel'
+echo_purple 'Creating A Folder for Laravel'
 sudo mkdir -p /var/www/laravel
 
+exit
 echo
 echo 'NOW WE CREATE SWAPFILE'
 
