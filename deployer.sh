3f60477851f20cb8a41cc384f5ad65157b99f0bf
Sidney_machara (Bad_Apple) <sidneymachara@gmail.com>
2019-05-08 11:34:12 +0200

    house cleaning

--- deployer.sh
+++ deployer.sh
@@ -11,20 +11,22 @@ source includes/format
 intro
 
 
-inform_human 'Updating server with (sudo apt-get update)'
+inform_human 'UPDATING SERVER (sudo apt-get update)'
 sudo apt-get update
 
-echo
-echo_purple 'installing Nginx'
+
+inform_human 'INSTALLING NGINX'
 sudo apt-get -y install nginx
 
-echo
-echo_purple  'installing mysql-server'
+
+inform_human 'INSTALLING mysql-server'
 sudo apt-get -y install mysql-server
 
-echo
-echo_purple 'sudo mysql_secure_installation'
+
+inform_human 'INSTALLING mysql_secure_installation'
 echo_white 'What we will do:'
+echo_white 'Re-enter password (No)'
+echo_white 'VALIDATOR ( No)'
 echo_white 'Remove anonymous users (yes)'
 echo_white 'Disallow root login remotely (yes)'
 echo_white 'Remove test database (yes)'
@@ -34,106 +36,86 @@ echo_purple '-------------------------'
 sudo mysql_secure_installation
 
 
-echo
-echo_purple 'install php-7.2 && most commonly used modules'
+inform_human 'INSTALLING php-7.2 && most commonly used modules'
 sudo apt-add-repository -y ppa:ondrej/php
 sudo apt-get update
 sudo apt-get -y --allow-unauthenticated install php7.2 php7.2-cli php7.2-common php7.2-json php7.2-opcache php7.2-mysql php7.2-mbstring  php7.2-zip php7.2-fpm php7.2-xml  zip unzip
 #php7.2-mcrypt <-- deprecated ??
-sudo apt-get -y php7.1-mcrypt
+sudo apt-get -y install php7.1-mcrypt
 
 
 
-
-echo
-echo_purple 'copying updated php.ini file (/la-deployer/php.ini /etc/php/7.2/fpm/php.ini) change made = (cgi.fix_pathinfo=0)'
+inform_human 'UPDATING php.ini (cgi.fix_pathinfo=0)' #
 sudo rm /etc/php/7.2/fpm/php.ini
-cp php.ini /etc/php/7.2/fpm/php.ini
+cp files/php.ini /etc/php/7.2/fpm/php.ini
 
-echo
-echo_purple 'restart php7.2-fpm'
+
+inform_human 'RESTARTING php7.2-fpm'
 sudo systemctl restart php7.2-fpm
 
 
 #if i can't find sudo nano /etc/nginx/sites-available/default -->re install nginx
-echo
-echo_purple 'copying updated /etc/nginx/sites-available/default'
+inform_human 'UPDATING /etc/nginx/sites-available/default'
 sudo rm /etc/nginx/sites-available/default
-cp default /etc/nginx/sites-available/default
+cp files/default /etc/nginx/sites-available/default
 
-echo
-echo_purple 'making sure it is error free'
+
+inform_human 'CHECKING FOR ERRORS IN default'
 sudo nginx -t
 
 
 
-echo
-echo_purple 'now to let it take effect you can restart Nginx'
+inform_human 'RESTARTING NGINX'
 sudo systemctl reload nginx
-
 sudo service nginx restart
 
 
-
-echo
-echo_purple 'Creating A Folder for Laravel in (/var/www/laravel)'
+inform_human 'CREATING A Folder for Laravel in (/var/www/laravel)'
 sudo mkdir -p /var/www/laravel
 
+inform_human'NOW WE CREATE SWAPFILE'
 
-echo
-echo_purple 'NOW WE CREATE SWAPFILE'
-
-echo
-echo_purple 'create 1gb swap file '
+inform_human 'CREATING 1GB SWAP FILE'
 sudo fallocate -l 1G /swapfile
 
-echo
-echo_purple 'tell Ubuntu to format it as swap space'
+inform_human 'TELL TO FORMAT IT AS SWAP SPACE && START USING IT'
 sudo mkswap /swapfile
-
-echo
-echo_purple 'now we start using it'
 sudo swapon /swapfile
 
 
-echo
-echo_purple 'INSTALLING COMPOSER'
+
+inform_human 'INSTALLING COMPOSER'
 cd ~
 curl -sS https://getcomposer.org/installer | php
 sudo mv composer.phar /usr/local/bin/composer
 
-echo
-echo_purple 'creating git repository in (/var/repo/site.git)'
+inform_human 'CREATING git repository in (/var/repo/site.git)'
 cd /var
 mkdir repo && cd repo
 
 mkdir site.git && cd site.git
 git init --bare
 
-echo
-echo_purple 'Setting Up Git Hooks'
+
+inform_human 'SETTING UP Git Hooks'
 cd hooks
 
-echo
-echo_purple 'copy post-receive file to /var/repo/site.git/hooks/'
-cp ~/la-deployer/post-receive /var/repo/site.git/hooks/
 
-echo
-echo_purple 'give post-recive file permissions to excute order to copy files over from git to sever'
+inform_human 'COPYING post-receive file to /var/repo/site.git/hooks/'
+cp ~/la-deployer/files/post-receive /var/repo/site.git/hooks/
+
+
+inform_human 'GIVING post-recive file permissions to excute order to copy files over from git to sever'
 sudo chmod +x /var/repo/site.git/hooks/post-receive
 
-echo
 
-echo_white "************************"
-echo_white "************************"
-echo
-echo_yellow 'go to your application and add the remote below,, replace 0.0.0.0 with your server ip'
-echo_yellow 'git remote add production ssh://root@0.0.0.0.0/var/repo/site.git'
-echo_yellow 'AND FOR THE MAGICAL PART'
-echo_purple 'git push production master'
-echo
-echo_white "************************"
-echo_white "************************"
+
+white_divider
+order_human 'go to your application and add the remote below,, replace 0.0.0.0 with your server ip'
+order_human 'git remote add production ssh://root@0.0.0.0.0/var/repo/site.git'
+inform_human 'AND FOR THE MAGICAL PART'
+order_human 'git push production master'
+white_divider
 
 echo
 echo
@@ -149,82 +131,75 @@ while true; do
   [Yy]* )
       #after pushing local app to server
 
-      echo
+      white_divider
+
       cd /var/www/laravel
-      echo_purple 'composer install --no-dev'
-      echo_white "*******************************************************"
-      echo_white "** if your composer.josn has errors, this will fail ***"
-      echo_white "*******************************************************"
+      inform_human 'RUNNING composer install --no-dev'
+      warn_human "*******************************************************"
+      warn_human "** if your composer.josn has errors, this will fail ***"
+      warn_human "*******************************************************"
       composer install --no-dev
 
-      echo
-      echo_purple 'creating env .file'
+
+      inform_human 'CREATING ..env file'
       cp /var/www/laravel/.env.example /var/www/laravel/.env
 
-      echo_purple 'generating app key'
+
+      inform_human 'GENERATING APP KEY'
       php artisan key:generate
 
-      echo
-      echo_purple 'give nginx permissionsover laravel directoy'
+
+      inform_human 'GIVING NGINX PERMISSION OVER LARAVEL DIRECTORY'
       sudo chown -R :www-data /var/www/laravel
       sudo chmod -R 775 /var/www/laravel/storage
       sudo chmod -R 775 /var/www/laravel/bootstrap/cache
 
 
-      echo
-      echo_red 'YOUR TURN -->Database Setup'
-      echo_red 'YOU HAVE TO ATLEAST DO SOMETHING'
-      echo_red 'Relax ill help you'
-      echo
-      echo_white "************************"
-      echo_white "************************"
-      echo
+      warn_human "*******************************************************"
+      warn_human 'YOUR TURN -->Database Setup'
+      warn_human 'YOU HAVE TO ATLEAST DO SOMETHING'
+      warn_human 'Relax ill help you'
+      warn_human "*******************************************************"
 
-      echo
-      echo_cyan 'type --> mysql -u root -p"yourpassword"'
-      echo_cyan 'type --> CREATE DATABASE your_db_name;'
-      echo_cyan 'type --> exit'
-      echo
-      echo_white "************************"
-      echo_white "************************"
+      white_divider
+      order_human 'type --> mysql -u root -p"yourpassword"'
+      order_human 'type --> CREATE DATABASE your_db_name;'
+      order_human 'type --> exit'
+      white_divider
 
       #will automate this part after i figure out how to (:P)
 
-      echo
-      echo_purple 'type --> sudo nano /var/www/laravel/.env'
-      echo_purple 'set up as you wish '
-      echo_purple 'TIP 1 -> APP_DEBUG=false'
-      echo_purple 'TIP 2 -> change --> database (name , password, user)'
-      echo_purple 'TIP 3 -> dont forget to put password in quotes ->"password"'
-      echo_purple 'SAVE'
-      echo
-      echo_white "************************"
-      echo_white "************************"
+      white_divider
+      order_human 'type --> sudo nano /var/www/laravel/.env'
+      inform_human 'set up as you wish '
+      order_human  'TIP 1 -> APP_DEBUG=false'
+      order_human  'TIP 2 -> DB_DATABASE= "YOUR_DATABASE_NAME"'
+      order_human  'TIP 3 -> DB_USERNAME= "YOUR_DATABASE_USERNAME"'
+      order_human  'TIP 4 -> DB_PASSWORD= "YOUR_PASSWORD" (dont forget to put password in quotes ->"YOUR_password") '
+      order_human  'SAVE ( CTRL + X , then Y , then ENTER)'
+      white_divider
 
 
-      echo
-      echo_purple 'type --> nano /var/www/laravel/config/app.php'
-      echo_purple ' change --> "url" => "http://example.com" to "url" => "http://your_server_ip"'
-      echo_purple 'SAVE'
-      echo
-      echo_white "************************"
-      echo_white "************************"
+      order_human 'type --> nano /var/www/laravel/config/app.php'
+      order_human ' change --> "url" => "http://example.com" to "url" => "http://your_server_ip"'
+      order_human 'SAVE ( CTRL + X , then Y , then ENTER)'
+      white_divider
 
 
-      echo
-      echo_purple 'running artisan config:cache'
-      echo_purple 'type --> cd /var/www/laravel'
-      echo_purple 'type --> php artisan config:cache'
-      echo
-      echo_purple 'running artisan migrate'
-      echo_purple 'type -->php artisan migrate'
-      echo
-      echo_white "************************"
-      echo_white "************************"
+      order_human 'type --> cd /var/www/laravel'
+      order_human 'type --> php artisan config:cache'
+      order_human 'type -->php artisan migrate'
+      white_divider
 
+      white_divider
+      white_divider
+      white_divider
 
+      inform_human 'ALL DONE'
 
-      echo_white 'ALL DONE'
+      white_divider
+      white_divider
+      white_divider
 
       break;;
 
